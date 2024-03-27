1f6e24b3a73e119a8ee6b1b78d1f3f8d060eb51f
Mansur Isaev <737dab2f169a@mail.ru>
2024-03-27 16:18:56 +0400

    Refactoring `InspectorProperty`

--- scripts/inspector_property.gd
+++ scripts/inspector_property.gd
@@ -2,15 +2,24 @@
 # See `LICENSE.md` included in the source distribution for details.
 
 ## Base InspectorProperty class.
-##
-## For inherited classes, override [method can_handle] and [method create_control] methods.
 class_name InspectorProperty
-extends RefCounted
+extends PanelContainer
 
 
 static var _declarations: Array[Dictionary] = []
 
-
+## Declares a supported type for properties.
+## [param Validation] must take two arguments [Object] and [Dictionary] and return [param true] if the property can be handled.
+## [codeblock] func can_handle(object: Object, property: Dictionary) -> bool:
+##     return property["type"] == TYPE_BOOL
+## [/codeblock]
+## [br][param Constructor] must take two arguments [Object] and [Dictionary] and return [Control].
+## [codeblock] func create_control(object: Object, property: Dictionary) -> Control:
+##     var label := Label.new()
+##     label.set_text(property["name"])
+##
+##     return label
+## [/codeblock]
 static func declare_property(validation: Callable, constructor: Callable) -> void:
 	assert(validation.is_valid(), "Invalid validation Callable.")
 	assert(constructor.is_valid(), "Invalid constructor Callable.")
@@ -18,7 +27,7 @@ static func declare_property(validation: Callable, constructor: Callable) -> voi
 	if validation.is_valid() and constructor.is_valid():
 		_declarations.push_front({"validation": validation, "constructor": constructor})
 
-
+## Create and returns a [Control] node for a property. If property is not supported returns [param null].
 static func create_property(object: Object, property: Dictionary) -> Control:
 	assert(is_instance_valid(object), "Invalid Object!")
 	if not is_instance_valid(object):
@@ -39,47 +48,93 @@ static func create_property(object: Object, property: Dictionary) -> Control:
 			return control
 
 	return null
-	
-	
-	
-# Magic numbers, but otherwise the SpinBox does not work correctly.
-const FLOAT_MIN = -999999999999.9
-const FLOAT_MAX =  999999999999.9
 
-## Return [param true] if [InspectorProperty] can handle the object and property.
-func can_handle(object: Object, property: Dictionary, readonly: bool) -> bool:
-	return false
 
-## Return [param true] if [InspectorProperty] is editable.
-## By default [param true] if [param NOT] [method Inspector.is_readonly].
-func is_editable(object: Object, property: Dictionary, readonly: bool) -> bool:
-	return not readonly
+var _object: Object = null
 
-## Factory method. Should be overridden.
-## Return [Control] for edit property value.
-func create_control(object: Object, property: Dictionary, readonly: bool) -> Control:
-	return null
+var _property: StringName = &""
+var _class_name: StringName = &""
+var _type: Variant.Type = TYPE_NIL
+var _hint: PropertyHint = PROPERTY_HINT_NONE
+var _hint_string: String = ""
+var _usage: int = PROPERTY_USAGE_NONE
 
-## Return [BoxContainer] with [Label] and custom [Control] as children.
-func create_combo_container(name: StringName, control: Control, vertical: bool = false) -> BoxContainer:
-	assert(is_instance_valid(control), "Invalid Control.")
-	if not is_instance_valid(control):
-		return null
 
+func _init(object: Object, property: Dictionary) -> void:
+	_object = object
+
+	_property = property["name"]
+	_class_name = property["class_name"]
+	_type = property["type"]
+	_hint = property["hint"]
+	_hint_string = property["hint_string"]
+	_usage = property["usage"]
+
+
+func get_object() -> Object:
+	return _object
+
+func get_property() -> StringName:
+	return _property
+
+func get_class_name() -> StringName:
+	return _class_name
+
+func get_type() -> Variant.Type:
+	return _type
+
+func is_compatible_type(type: Variant.Type) -> bool:
+	return get_type() == type
+
+func get_hint() -> PropertyHint:
+	return _hint
+
+func get_hint_string() -> String:
+	return _hint_string
+
+func get_usage() -> PropertyUsageFlags:
+	return _usage
+
+
+func set_value(new_value: Variant) -> void:
+	get_object().set(get_property(), new_value)
+
+func get_value() -> Variant:
+	return get_object().get(get_property())
+
+func set_and_return_value(new_value: Variant) -> Variant:
+	set_value(new_value)
+	return get_value()
+
+## Returns created child [BoxContainer] node with [Label] and custom [Control] as children.
+func create_combo_container(title: String, control: Control, vertical := false) -> BoxContainer:
 	var container := BoxContainer.new()
-	container.vertical = vertical
+	container.set_vertical(vertical)
+	container.set_name("Container")
 
-	var label = Label.new()
-	label.text = tr(name).capitalize()
-	label.tooltip_text = label.text
-	label.mouse_filter = Control.MOUSE_FILTER_STOP
-	label.text_overrun_behavior = TextServer.OVERRUN_TRIM_ELLIPSIS
-	label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	label.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	container.add_child(label)
+	if title:
+		var label := Label.new()
+		label.set_name("Label")
+		label.set_vertical_alignment(VERTICAL_ALIGNMENT_CENTER)
+		label.set_text(title.capitalize())
+		label.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
+		label.set_text_overrun_behavior(TextServer.OVERRUN_TRIM_ELLIPSIS)
+		label.set_h_size_flags(Control.SIZE_EXPAND_FILL)
+		label.set_v_size_flags(Control.SIZE_EXPAND_FILL)
+		label.set_stretch_ratio(0.75)
+		label.set_custom_minimum_size(Vector2(192/2, 16.0))
+		container.add_child(label)
 
-	control.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	control.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	container.add_child(control)
+	if is_instance_valid(control):
+		control.set_name("Property")
+		control.set_h_size_flags(Control.SIZE_EXPAND_FILL)
+		control.set_v_size_flags(Control.SIZE_EXPAND_FILL)
+		container.add_child(control)
+
+	self.add_child(container)
 
 	return container
+
+## Return [param true] if [InspectorProperty] can handle the object and property.
+static func can_handle(object: Object, property: Dictionary) -> bool:
+	return false
