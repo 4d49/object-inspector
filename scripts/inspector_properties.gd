5bf46fc79f7e0efba67e5282a2b274bc956a9810
Mansur Isaev <737dab2f169a@mail.ru>
2024-05-19 20:51:00 +0400

    `InspectorPropertyString` add support file & dir

--- scripts/inspector_properties.gd
+++ scripts/inspector_properties.gd
@@ -92,7 +92,7 @@ class InspectorPropertyBool extends InspectorProperty:
 
 	func _init(object: Object, property: Dictionary, editable: bool, setter: Callable, getter: Callable) -> void:
 		super(object, property, editable, setter, getter)
-		
+
 		check_box = create_bool_control(set_value, get_value, is_editable())
 		create_flow_container(property["name"], check_box)
 
@@ -177,15 +177,64 @@ class InspectorPropertyNumber extends InspectorProperty:
 ## Handle [String] or [StringName] property.
 class InspectorPropertyString extends InspectorProperty:
 	var line_edit: LineEdit = null
+	var choose_file: Button = null
 
 	func _init(object: Object, property: Dictionary, editable: bool, setter: Callable, getter: Callable) -> void:
 		super(object, property, editable, setter, getter)
+
 		if property["type"] == TYPE_STRING:
 			line_edit = create_string_control(set_value, get_value, editable)
 		else:
 			line_edit = create_string_name_control(set_value, get_value, editable)
 
-		create_flow_container(property["name"], line_edit)
+		var hint: PropertyHint = property["hint"]
+		if not is_hint_file_or_dir(hint):
+			create_flow_container(property["name"], line_edit)
+			return
+
+		line_edit.set_h_size_flags(Control.SIZE_EXPAND_FILL)
+
+		var hbox := HBoxContainer.new()
+		hbox.add_child(line_edit)
+		hbox.set_h_size_flags(Control.SIZE_EXPAND_FILL)
+
+		var hint_string: String = property["hint_string"]
+
+		setter = func(text: String) -> void:
+			line_edit.set_text(set_and_return_value(text))
+
+		choose_file = Button.new()
+		choose_file.pressed.connect(func() -> void:
+			var file_dialog := FileDialog.new()
+
+			match hint:
+				PROPERTY_HINT_FILE, PROPERTY_HINT_GLOBAL_FILE:
+					file_dialog.set_access(FileDialog.ACCESS_RESOURCES if hint == PROPERTY_HINT_FILE else FileDialog.ACCESS_FILESYSTEM)
+					file_dialog.set_current_path(get_value())
+					file_dialog.set_file_mode(FileDialog.FILE_MODE_OPEN_FILE)
+					file_dialog.file_selected.connect(setter)
+				_:
+					file_dialog.set_access(FileDialog.ACCESS_RESOURCES if hint == PROPERTY_HINT_DIR else FileDialog.ACCESS_FILESYSTEM)
+					file_dialog.set_current_dir(get_value())
+					file_dialog.set_file_mode(FileDialog.FILE_MODE_OPEN_DIR)
+					file_dialog.dir_selected.connect(setter)
+
+			file_dialog.add_filter(hint_string)
+			# Free after hide.
+			file_dialog.visibility_changed.connect(func() -> void:
+				if not file_dialog.is_visible():
+					file_dialog.queue_free()
+			)
+			self.add_child(file_dialog)
+
+			file_dialog.popup_centered_ratio(0.5)
+		)
+		hbox.add_child(choose_file)
+		create_flow_container(property["name"], hbox)
+
+	func _enter_tree() -> void:
+		if is_instance_valid(choose_file):
+			choose_file.set_button_icon(get_theme_icon(&"file", &"Inspector"))
 
 	static func _static_init() -> void:
 		InspectorPropertyType.register_type(TYPE_STRING, "String", create_string_control)
@@ -221,6 +270,10 @@ class InspectorPropertyString extends InspectorProperty:
 
 	static func create_string_name_control(setter: Callable, getter: Callable, editable: bool) -> LineEdit:
 		return _create_line_edit(setter, getter, editable, true)
+
+	static func is_hint_file_or_dir(hint: PropertyHint) -> bool:
+		return hint == PROPERTY_HINT_FILE or hint == PROPERTY_HINT_DIR or\
+			hint == PROPERTY_HINT_GLOBAL_FILE or hint == PROPERTY_HINT_GLOBAL_DIR
 
 	static func can_handle(_object: Object, property: Dictionary, _editable: bool) -> bool:
 		return property["type"] == TYPE_STRING or property["type"] == TYPE_STRING_NAME
