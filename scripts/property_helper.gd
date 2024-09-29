352a523e9ce63cc3655f8b93cf847f260a0d6849
Mansur Isaev <737dab2f169a@mail.ru>
2024-09-29 13:10:49 +0400

    Add `PropertyHelper` class

---
+++ scripts/property_helper.gd
@@ -0,0 +1,91 @@
+# Copyright (c) 2022-2024 Mansur Isaev and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+class_name PropertyHelper
+extends RefCounted
+
+
+var _setter_map: Dictionary[StringName, Callable] = {}
+var _getter_map: Dictionary[StringName, Callable] = {}
+
+var _property_list: Array[Dictionary] = []
+
+
+func _set(property: StringName, value: Variant) -> bool:
+	var setter: Callable = _setter_map.get(property, Callable())
+	return setter.is_valid() and setter.call(value) != false
+
+func _get(property: StringName) -> Variant:
+	var getter: Callable = _getter_map.get(property, Callable())
+	if getter.is_valid():
+		return getter.call()
+
+	return null
+
+
+func _get_property_list() -> Array[Dictionary]:
+	return _property_list
+
+
+static func create_property(
+		name: String,
+		type: Variant.Type,
+		hint: PropertyHint,
+		hint_string: String,
+		usage: int,
+	) -> Dictionary[String, Variant]:
+
+	var property: Dictionary[String, Variant] = {
+		"name": name,
+		"type": type,
+		"hint": hint,
+		"hint_string": hint_string,
+		"usage": usage,
+	}
+	property.make_read_only()
+
+	return property
+
+
+func add_category(name: String) -> void:
+	_property_list.push_back(create_property(name, TYPE_NIL, PROPERTY_HINT_NONE, "", PROPERTY_USAGE_CATEGORY))
+	property_list_changed.emit()
+
+
+func add_group(name: String) -> void:
+	_property_list.push_back(create_property(name, TYPE_NIL, PROPERTY_HINT_NONE, "", PROPERTY_USAGE_GROUP))
+	property_list_changed.emit()
+
+
+func add_property(
+		name: StringName,
+		type: Variant.Type,
+		setter: Callable,
+		getter: Callable,
+		hint: PropertyHint = PROPERTY_HINT_NONE,
+		hint_string: String = "",
+		usage: int = PROPERTY_USAGE_DEFAULT + PROPERTY_USAGE_SCRIPT_VARIABLE,
+	) -> bool:
+
+	if _setter_map.has(name):
+		return false
+
+	_setter_map[name] = setter
+	_getter_map[name] = getter
+
+	_property_list.push_back(create_property(name, type, hint, hint_string, usage))
+	property_list_changed.emit()
+
+	return true
+
+
+static func range_to_hint_string(min: float, max: float, step: float) -> String:
+	return String.num(min) + "," + String.num(max) + "," + String.num(step)
+
+static func enum_to_hint_string(enumeration: Dictionary) -> String:
+	var hint_string: String = ""
+
+	for key: String in enumeration:
+		hint_string += key + ":" + String.num_int64(enumeration[key]) + ","
+
+	return hint_string.left(-1)
