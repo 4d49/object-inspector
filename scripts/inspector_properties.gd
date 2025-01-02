9c9c8fe72778b9f997e16a2898fe229fd1700e5b
Mansur Isaev <737dab2f169a@mail.ru>
2025-01-02 15:13:28 +0400

    Implement `InspectorPropertyButton` class

--- scripts/inspector_properties.gd
+++ scripts/inspector_properties.gd
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2024 Mansur Isaev and contributors - MIT License
+# Copyright (c) 2022-2025 Mansur Isaev and contributors - MIT License
 # See `LICENSE.md` included in the source distribution for details.
 
 # Magic numbers, but otherwise the SpinBox does not work correctly.
@@ -85,6 +85,42 @@ class InspectorPropertySubgroup extends InspectorPropertyGroup:
 
 	static func can_handle(_obj: Object, property: Dictionary) -> bool:
 		return property["usage"] == PROPERTY_USAGE_SUBGROUP
+
+## Handle [annotation @GDScript.@export_tool_button] property.
+class InspectorPropertyButton extends InspectorProperty:
+	var button: Button = null
+
+	func _init(object: Object, property: Dictionary, setter: Callable, getter: Callable) -> void:
+		super(object, property, setter, getter)
+
+		button = Button.new()
+		button.set_tooltip_text(get_property_description(object, property.name))
+		button.set_theme_type_variation(&"InspectorPropertyButton")
+
+		var hint_split: PackedStringArray = String(property.hint_string).split(',')
+		button.set_text(get_button_text(property.name, hint_split))
+		button.set_button_icon(get_button_icon(hint_split))
+
+		# Callable is stored as a variable, so to get it, we must call getter.
+		var callback: Callable = getter.call()
+		if callback.is_valid():
+			button.pressed.connect(callback)
+		else:
+			button.set_disabled(true)
+
+		self.add_child(button)
+
+	static func can_handle(_obj: Object, property: Dictionary) -> bool:
+		return property.hint == PROPERTY_HINT_TOOL_BUTTON and property.type == TYPE_CALLABLE
+
+	static func get_button_text(name: String, hint_split: PackedStringArray) -> String:
+		return hint_split[0] if not hint_split.is_empty() and not hint_split[0].is_empty() else name
+
+	static func get_button_icon(hint_split: PackedStringArray) -> Texture2D:
+		if hint_split.size() > 1 and ResourceLoader.exists(hint_split[1], "Texture2D"):
+			return ResourceLoader.load(hint_split[1], "Texture2D")
+
+		return null
 
 ## Handle [bool] property.
 class InspectorPropertyBool extends InspectorProperty:
@@ -651,6 +687,7 @@ static func _static_init() -> void:
 	InspectorProperty.declare_property(InspectorPropertyCategory.can_handle, InspectorPropertyCategory.new)
 	InspectorProperty.declare_property(InspectorPropertyGroup.can_handle, InspectorPropertyGroup.new)
 	InspectorProperty.declare_property(InspectorPropertySubgroup.can_handle, InspectorPropertySubgroup.new)
+	InspectorProperty.declare_property(InspectorPropertyButton.can_handle, InspectorPropertyButton.new)
 	InspectorProperty.declare_property(InspectorPropertyBool.can_handle, InspectorPropertyBool.new)
 	InspectorProperty.declare_property(InspectorPropertyNumber.can_handle, InspectorPropertyNumber.new)
 	InspectorProperty.declare_property(InspectorPropertyString.can_handle, InspectorPropertyString.new)
